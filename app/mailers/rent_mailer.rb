979385461ea0c7d8fbc415d2d52f3e416eff8c61
Gonzalo Escandarini <gonziesc@hotmail.com>
2017-09-12 18:05:13 -0300

    deleting comment and unecessary file

--- app/mailers/rent_mailer.rb
+++ app/mailers/rent_mailer.rb
@@ -1,9 +1,4 @@
 class RentMailer < ApplicationMailer
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.rent_mailer.new_record_notification.subject
-  #
   def new_record_notification(rent)
     @rent = rent
     @today = Time.zone.today
