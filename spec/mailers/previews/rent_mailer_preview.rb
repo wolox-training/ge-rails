55538607802a1526720e2ae0ce87dd31baf9217f
Gonzalo Escandarini <gonziesc@hotmail.com>
2017-09-12 17:10:36 -0300

    deleting autogenerate tests

--- spec/mailers/previews/rent_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/rent_mailer
-class RentMailerPreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/rent_mailer/new_record_notification
-  def new_record_notification
-    RentMailerMailer.new_record_notification
-  end
-
-end
