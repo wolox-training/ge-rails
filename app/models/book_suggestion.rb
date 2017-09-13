78c90a4c06dcf76c588cead8077d0f8e755df83f
Gonzalo Escandarini <gonziesc@hotmail.com>
2017-09-13 14:25:57 -0300

    adding g to suggestions

--- app/models/book_suggestion.rb
+++ app/models/book_suggestion.rb
@@ -1,4 +1,4 @@
-class BookSugestion < ApplicationRecord
+class BookSuggestion < ApplicationRecord
   belongs_to :user
   validates :author, :title, :link, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170913151427) do
+ActiveRecord::Schema.define(version: 20170913172348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "active_admin_comments", force: :cascade do |t|
-    t.string "namespace"
-    t.text "body"
-    t.string "resource_type"
-    t.bigint "resource_id"
-    t.string "author_type"
-    t.bigint "author_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
-    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
-    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
-  end
 
   create_table "admin_users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -46,7 +32,7 @@ ActiveRecord::Schema.define(version: 20170913151427) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
-  create_table "book_sugestions", force: :cascade do |t|
+  create_table "book_suggestions", force: :cascade do |t|
     t.string "editorial"
     t.float "price"
     t.string "author"
@@ -57,7 +43,7 @@ ActiveRecord::Schema.define(version: 20170913151427) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
-    t.index ["user_id"], name: "index_book_sugestions_on_user_id"
+    t.index ["user_id"], name: "index_book_suggestions_on_user_id"
   end
 
   create_table "books", force: :cascade do |t|
@@ -103,7 +89,7 @@ ActiveRecord::Schema.define(version: 20170913151427) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "book_sugestions", "users"
+  add_foreign_key "book_suggestions", "users"
   add_foreign_key "rents", "books"
   add_foreign_key "rents", "users"
 end
