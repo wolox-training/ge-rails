5f8401fc01e284b100a6c3f60bd832c14bcbb9c1
Gonzalo Escandarini <gonziesc@hotmail.com>
2017-09-18 14:37:07 -0300

    adding google config to secret

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -9,4 +9,5 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     session['devise.google_data'] = request.env['omniauth.auth'].except(:extra)
     redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
   end
+  # rubocop:enable Metrics/AbcSize
 end
